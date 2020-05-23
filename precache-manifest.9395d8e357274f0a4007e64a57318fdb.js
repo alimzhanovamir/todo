98629c964f6f106d929d7d12fec41457e55c77d4
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-05-23 20:50:16 +0300

    Updates

---
+++ precache-manifest.9395d8e357274f0a4007e64a57318fdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d5c233f91382f24c5d3ab290057774",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "35f97ff10f9f2a23c3f6",
+    "url": "/todo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b62b8a7071e68b270122",
+    "url": "/todo/static/js/2.87eb8b5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.87eb8b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35f97ff10f9f2a23c3f6",
+    "url": "/todo/static/js/main.b02b2eb3.chunk.js"
+  },
+  {
+    "revision": "42e23f9118876525bea8",
+    "url": "/todo/static/js/runtime-main.0d77e128.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
