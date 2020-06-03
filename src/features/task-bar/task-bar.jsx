fce91bc8666c2bef6102ba2522a3873f0cad1044
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-06-03 18:21:03 +0300

    rename toolbar to task-bar, add ui and selector

---
+++ src/features/task-bar/task-bar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Panel } from '@ui/organisms/panel';
+import { useSelector, useDispatch } from 'react-redux';
+
+export const TaskBar = (props) => {
+  const {
+    sortValues, 
+    sortValue, 
+    sortByTypeValues, 
+    sortByTypeValue, 
+    searchValue
+  } = useSelector( ({listReducer}) => listReducer );
+
+  const dispatch = useDispatch();
+  
+  return (
+    <Panel
+      searchValue={searchValue}
+      sortValues={sortValues}
+      sortValue={sortValue}
+      sortByTypeValues={sortByTypeValues}
+      sortByTypeValue={sortByTypeValue}
+      dispatch={dispatch}
+    />
+  );
+}
--- src/features/toolbar/index.js
+++
@@ -1,1 +0,0 @@
-export { tollbar } from './toolbar';
