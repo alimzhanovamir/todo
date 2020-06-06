2b27aacc888219107675a9501e8da145257a6437
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-06-06 22:11:08 +0300

    subscribe localStorage

--- src/model/model.js
+++ src/model/model.js
@@ -5,3 +5,17 @@ import { modalReducer } from '@features/modal';
 const reducers = combineReducers({listReducer, modalReducer})
 
 export const model = createStore(reducers);
+
+let prevList = model.getState().listReducer.list;
+
+const saveToLocalStorage = () => {
+  const list = model.getState().listReducer.list;
+  
+  if ( prevList !== list ) {
+    prevList = list
+    localStorage.setItem('todoList', JSON.stringify(list));
+  }
+  
+}
+
+model.subscribe(saveToLocalStorage);
