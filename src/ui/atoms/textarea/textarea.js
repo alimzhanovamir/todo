524216bf13c31c5f0a48b28fcc29fb9621d299e4
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-06-06 22:06:43 +0300

    create textarea component

---
+++ src/ui/atoms/textarea/textarea.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Textarea = styled.textarea`
+  width: 100%;
+  height: 100px;
+  background-color: #1e1d1c;
+  border-radius: 3px;
+  border: none;
+  padding: 8px;
+  color: white;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 14px;
+  line-height: 1em;
+  outline: none;
+  cursor: pointer;
+
+  transition: box-shadow .3s;
+
+  &:focus {
+    box-shadow: 0px 1px 5px 0px #3e3e3e;
+  }
+`;
