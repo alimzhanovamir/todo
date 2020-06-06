c4e4a0b25e6f4d92e74c1c8b0a2f1bea5c3fb0e0
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-06-06 22:11:50 +0300

    add edit state

--- src/features/modal/modal.jsx
+++ src/features/modal/modal.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { Button } from '@ui/atoms/button';
 import { Input } from '@ui/atoms/input';
+import { Textarea } from '@ui/atoms/textarea';
 import { closeModal } from './actions/actions';
 import { addTask } from '@features/task-list';
 
@@ -36,10 +38,14 @@ const ModalElementHeader = styled.p`
 
 const ModalElementForm = styled.p`
   display: flex;
-  margin: 0;
+  margin: 0 0 15px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
 
   ${Button} {
-    margin-left: 10px;
+    margin-left: auto;
   }
 `;
 
@@ -48,7 +54,9 @@ const ModalElementTitle = styled.span`
 `;
 
 export const Modal = (props) => {
+  const {title, text} = useSelector( ({ listReducer }) => listReducer.list.find( task => task.id === id ) )
   const [inputValue, setInputValue] = useState('');
+  const [textareaValue, setTextareaValue] = useState('');
   const isOpen = useSelector( ({modalReducer: { isOpen }}) => isOpen );
   const editMode = useSelector( ({listReducer: { editMode }}) => editMode);
   const dispatch = useDispatch();
@@ -66,15 +74,21 @@ export const Modal = (props) => {
           }}>&#10060;</Button>
         </ModalElementHeader>
         <ModalElementForm>
-          <Input value={inputValue} onChange={ e => setInputValue(e.target.value) }/>
+          <Input value={inputValue} onChange={ e => setInputValue(e.target.value) } autoFocus={true}/>
+        </ModalElementForm>
+        <ModalElementForm>
+          <Textarea value={textareaValue} onChange={ e => setTextareaValue(e.target.value) }/>
+        </ModalElementForm>
+        <ModalElementForm>
           <Button 
-            disabled={ inputValue ? false : true }
+            disabled={ inputValue && textareaValue ? false : true }
             onClick={ () => {
-              dispatch(addTask(inputValue));
+              dispatch(addTask({title: inputValue, text: textareaValue}));
               dispatch(closeModal());
-              setInputValue('')
+              setInputValue('');
+              setTextareaValue('');
             }}
-          >&#9989;</Button>
+          >&#9989; Add</Button>
         </ModalElementForm>
       </ModalElementInner>
     </ModalElement>
