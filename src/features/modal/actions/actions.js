90ac835ba1497cd6d414cc5e96e8969755fb7296
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-06-07 23:03:09 +0300

    added data transfer to reduce task change call code

--- src/features/modal/actions/actions.js
+++ src/features/modal/actions/actions.js
@@ -1,8 +1,9 @@
 export const OPEN_MODAL = 'OPEN_MODAL';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
 
-export const openModal = () => ({
-  type: OPEN_MODAL
+export const openModal = (payload) => ({
+  type: OPEN_MODAL,
+  payload
 });
 
 export const closeModal = () => ({
