ee3179cfc8603654f1f4ce51624670e371500757
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-06-12 00:34:40 +0300

    add propTypes & default props

--- src/ui/molecules/form-field/form-field.jsx
+++ src/ui/molecules/form-field/form-field.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { FormFieldElement } from './styles';
 import { Input, Select, Label, Button } from '@ui';
 
 export const FormField = (props) => {
-  const {controlType, label = '', id = '', options = [], text = ''} = props;
+  const { controlType, label, id , options, text } = props;
   
   const selectControl = (controlType) => {
     switch (controlType) {
       case 'input':
         return <Input {...props}/>
       case 'select':
-        return <Select {...props}>
-                 {options.map( ({value, label}) => <option value={value} key={value}>{label}</option>)}
-               </Select>
+        return (
+          <Select {...props}>
+            {options.map( ({value, label}) => <option value={value} key={value}>{label}</option>)}
+          </Select>)
       case 'button': 
         return <Button {...props}>{text}</Button>
       default:
@@ -27,3 +29,21 @@ export const FormField = (props) => {
     </FormFieldElement>
   );
 }
+
+FormField.propTypes = {
+  controlType: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  options: PropTypes.array.isRequired,
+  text: PropTypes.string.isRequired,
+  id: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]).isRequired
+}
+
+FormField.defaultProps = {
+  label: '',
+  text: '',
+  id: '',
+  options: []
+};
